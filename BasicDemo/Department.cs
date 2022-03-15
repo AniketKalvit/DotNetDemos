70d363f9ab9f162d694e797630957cbdc6d0eaaa
Aniket Kalvit <aniket.kalvit@9ledgepro.com>
2022-03-15 09:07:12 +0530

    Add project files.

---
+++ BasicDemo/Department.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicDemo // project 1
+{
+    public class Department
+    {
+        private int id;
+        private string name, location;
+        public Department()
+        {
+            id = 1;
+            name = "Admin";
+            location = "Pune";
+        }
+        public Department(int did,string dname,string loc)
+        {
+            id = did;
+            name = dname;
+            location = loc;
+        }
+       public int  GetDeptId()
+        {
+            return id;
+        }
+        //public void AssignData()
+        //{
+        //    id = 1;
+        //    name = "Admin";
+        //    location = "Pune";
+        //}
+        public string GetData()
+        {
+            return "Dept Id " + id + "Dept name " + name + " location " + location;
+           
+        }
+    }
+  
+}
---
+++ BasicDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;  //  FCL
+
+namespace BasicDemo  // as container - assembly  project
+{
+    // is a collection of Types  ( class, struct, enum, delegate, interfaces....)
+    class Program
+    {
+        // members of class / struct / interface
+        // variable(data member), methods, constructor,events , properties
+        static void Main(string[] args) // entry point of program
+        {
+
+            // create object of Department class
+            Department dept = new Department();
+            int i1=dept.GetDeptId();
+            Console.WriteLine(dept.GetData());
+
+            Department dept2 = new Department(2, "HR", "Mumbai");
+            Console.WriteLine(dept2.GetDeptId());
+            Console.WriteLine(dept2.GetData());
+
+            Department d = new Department();
+        }
+
+
+    }
+
+
+}
